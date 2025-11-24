198334f5429beb8cb104a2ba3479e8579433bf34
Colourless <205353678+AcrylonitrileButadieneStyrene@users.noreply.github.com>
2025-11-24 14:54:43 -0500

    remove directory picker, parallelize (6x speedup), progress bar

--- src/main.rs
+++ src/main.rs
@@ -3,13 +3,13 @@
 #![allow(clippy::cast_possible_truncation)]
 #![allow(clippy::missing_panics_doc)]
 
+use indicatif::ParallelProgressIterator as _;
 use lcf::ConvertExt;
-use owo_colors::OwoColorize;
-
-mod directory_browser;
-mod lints;
-
 pub use lints::{Diagnostic, DiagnosticEvent, DiagnosticLevel, DiagnosticPage, Lint};
+use owo_colors::OwoColorize;
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
+
+mod lints;
 
 #[derive(Clone, Debug, PartialEq, Eq, clap::ValueEnum)]
 pub enum LogLevel {
@@ -22,9 +22,6 @@ pub enum LogLevel {
 struct Args {
     #[arg(index = 1, default_value = ".")]
     path: std::path::PathBuf,
-    /// Check every map file in a game.
-    #[arg(long)]
-    all: bool,
     /// Minimum level for logging, everything lower will be ignored.
     #[arg(long, default_value = "all")]
     level: LogLevel,
@@ -36,38 +33,16 @@ struct Args {
 fn main() {
     let args = <Args as clap::Parser>::parse();
 
-    if std::fs::metadata(&args.path).unwrap().is_dir() {
-        let Some(path) = find_game_dir(args.path) else {
-            println!("Failed to find a game from the given directory");
-            exit();
-        };
-
-        if args.all {
-            let tree = lcf::lmt::LcfMapTree::read(&mut std::io::Cursor::new(
-                std::fs::read(path.join("RPG_RT.lmt")).unwrap(),
-            ))
-            .unwrap();
-
-            for (id, _) in &tree.maps[1..] {
-                check_map(
-                    &path.join(format!("Map{id:04}.lmu")),
-                    &args.level,
-                    &args.ignore,
-                );
-            }
-        } else {
-            check_map(&directory_browser::run(&path), &args.level, &args.ignore);
-        }
+    let target = if std::fs::metadata(&args.path).unwrap().is_dir() {
+        let tree = read_tree(&args.path);
+        either::Either::Right((args.path, tree))
     } else {
         match args.path.extension().and_then(std::ffi::OsStr::to_str) {
-            Some("ldb" | "lmt") => {
-                check_map(
-                    &directory_browser::run(args.path.parent().unwrap()),
-                    &args.level,
-                    &args.ignore,
-                );
-            }
-            Some("lmu") => check_map(&args.path, &args.level, &args.ignore),
+            Some("lmt" | "ldb") => either::Either::Right((
+                args.path.parent().unwrap().to_owned(),
+                read_tree(&args.path.with_extension("lmt")),
+            )),
+            Some("lmu") => either::Either::Left(args.path),
             x => {
                 println!(
                     "Unrecognized extension {} is not supported.",
@@ -76,51 +51,79 @@ fn main() {
                 exit();
             }
         }
-    }
+    };
+
+    match target {
+        either::Either::Left(single) => analyze(
+            read_map(&single).unwrap().unwrap(),
+            &args.level,
+            &args.ignore,
+        )
+        .for_each(|line| println!("{}", line)),
+        either::Either::Right((base, tree)) => {
+            let mut maps = tree.maps[1..]
+                .into_par_iter()
+                .progress_with(
+                    indicatif::ProgressBar::new(tree.maps.len() as u64 - 1).with_style(
+                        indicatif::ProgressStyle::default_bar()
+                            .template(
+                                "[{elapsed} / {duration}] {bar:40.cyan/blue} {pos:>4}/{len:4} ({percent}%) {per_sec:>0}",
+                            )
+                            .unwrap(),
+                    ),
+                )
+                .map(
+                    |(id, _)| match read_map(&base.join(format!("Map{id:04}.lmu"))) {
+                        Ok(Ok(map)) => (
+                            id,
+                            either::Right(
+                                analyze(map, &args.level, &args.ignore).collect::<Vec<_>>(),
+                            ),
+                        ),
+                        Ok(Err(err)) => (
+                            id,
+                            either::Left(format!(
+                                "Map{id:04}.lmu\n  {}: {}",
+                                "Invalid map file".on_red(),
+                                err.red()
+                            )),
+                        ),
+                        Err(err) => (id, either::Left(format!("Map{id:04}.lmu\n  {}", err.red()))),
+                    },
+                )
+                .collect::<Vec<_>>();
+            maps.sort_by_key(|item| *item.0);
+            maps.iter().for_each(|(id, result)| match result {
+                either::Left(err) => println!("{}", err),
+                either::Right(output) => {
+                    println!("Map{id:04}.lmu:");
+                    for line in output {
+                        println!("  {}", line);
+                    }
+                }
+            });
+        }
+    };
 
     exit();
 }
 
-fn find_game_dir(base: std::path::PathBuf) -> Option<std::path::PathBuf> {
-    if base.join("RPG_RT.ldb").exists() {
-        Some(base)
-    } else {
-        let parent = base.parent()?;
-        if parent.join("RPG_RT.ldb").exists() {
-            Some(parent.to_owned())
-        } else {
-            None
-        }
-    }
+fn read_tree(path: &std::path::Path) -> lcf::lmt::LcfMapTree {
+    let bytes = std::fs::read(path.join("RPG_RT.lmt")).unwrap();
+    lcf::lmt::LcfMapTree::read(&mut std::io::Cursor::new(bytes)).unwrap()
 }
 
-fn check_map(map: &std::path::Path, level: &LogLevel, ignored: &[usize]) {
-    let print_file = || println!("{}:", map.file_name().unwrap().to_str().unwrap());
-
-    let data = match std::fs::read(map) {
-        Ok(data) => data,
-        Err(err) => {
-            print_file();
-            println!("  {}", err.red());
-            return;
-        }
-    };
-
-    let map = match lcf::lmu::LcfMapUnit::read(&mut std::io::Cursor::new(data)) {
-        Ok(map) => map,
-        Err(err) => {
-            print_file();
-            println!("  {}: {}", "Invalid map file".on_red(), err.red());
-            return;
-        }
-    };
-
-    let results = lints::ALL
+fn analyze(
+    map: lcf::lmu::LcfMapUnit,
+    level: &LogLevel,
+    ignored: &[usize],
+) -> impl Iterator<Item = String> {
+    lints::ALL
         .iter()
         .enumerate()
         .filter(|(index, _)| !ignored.iter().any(|ignore| *ignore == index + 1))
-        .map(|(index, lint)| (index + 1, lint.name(), lint.test(&map)))
-        .filter_map(|(index, name, diagnostics)| match level {
+        .map(move |(index, lint)| (index + 1, lint.name(), lint.test(&map)))
+        .filter_map(move |(index, name, diagnostics)| match level {
             LogLevel::All => Some((index, name, diagnostics)),
             LogLevel::Warn => {
                 if diagnostics.is_empty() {
@@ -141,28 +144,36 @@ fn check_map(map: &std::path::Path, level: &LogLevel, ignored: &[usize]) {
                 }
             }
         })
-        .collect::<Vec<_>>();
-
-    if !results.is_empty() {
-        print_file();
-        for (index, name, diagnostics) in results {
+        .flat_map(|(index, name, diagnostics)| {
             if diagnostics.is_empty() {
-                println!("  L{index:04}: {}", name.green());
+                vec![format!("L{index:04}: {}", name.green())]
             } else {
-                println!("  L{index:04}: {name}:");
-                for diagnostic in diagnostics {
-                    match diagnostic.level {
-                        DiagnosticLevel::Warning => println!("    {}", diagnostic.yellow()),
-                        DiagnosticLevel::Error => println!("    {}", diagnostic.red()),
-                    }
-                }
+                let mut items = Vec::with_capacity(diagnostics.len() + 1);
+                items.push(format!("L{index:04}: {name}:"));
+                items.extend(
+                    diagnostics
+                        .into_iter()
+                        .map(|diagnostic| match diagnostic.level {
+                            DiagnosticLevel::Warning => format!("  {}", diagnostic.yellow()),
+                            DiagnosticLevel::Error => format!("  {}", diagnostic.red()),
+                        }),
+                );
+                items
             }
-        }
-    }
+        })
+}
+
+fn read_map(
+    path: &std::path::Path,
+) -> Result<Result<lcf::lmu::LcfMapUnit, lcf::lmu::LcfMapUnitReadError>, std::io::Error> {
+    let bytes = std::fs::read(path)?;
+    let mut cursor = std::io::Cursor::new(bytes);
+    let map = lcf::lmu::LcfMapUnit::read(&mut cursor);
+    Ok(map)
 }
 
 fn exit() -> ! {
-    if atty::is(atty::Stream::Stdin) {
+    if atty::is(atty::Stream::Stdin) && atty::is(atty::Stream::Stdout) {
         eprint!("Press enter to exit...");
         std::io::Write::flush(&mut std::io::stdout()).unwrap();
         drop(std::io::stdin().read_line(&mut String::new()));
